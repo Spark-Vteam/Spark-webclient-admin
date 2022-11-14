af334e452bbd8c8f21f81549de27b5ab35ba7ebb
emcofah <emfh21@student.bth.se>
2022-11-14 14:13:06 +0100

    added users to admin client

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import cell from "../img/cell.png";
 import logo from "../img/logo-admin.png";
-import spark from "../img/heading.png";
-
-function handleClick() {
-  fetch("http://localhost:4000/")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  function fetchUsers() {
+    fetch("http://localhost:4000/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }
+
+  useEffect(() => {
+    (async () => {
+      await fetchUsers();
+    })();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
   return (
     <div>
       <div className="topnav">
@@ -26,10 +36,22 @@ function Users() {
           </Link>
         </div>
       </div>
-      <div className="container">
-        <button className="btn" onClick={handleClick}>
-          Fetch data admin
-        </button>
+      <div>
+        <div className="container">
+          {users.map((user) => {
+            return (
+              <div key={user.id}>
+                <h2>
+                  <strong>
+                    {user.FirstName} {user.LastName}
+                  </strong>
+                </h2>
+                <p>{user.EmailAdress}</p>
+                <hr />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
